d49b03b9bb3bffb55251fa68287ea8dc54cc2bde
poorvabhole <poorva.bhole@cognologix.com>
2024-07-13 23:56:18 +0530

    Linked-list-append

--- DSA_Practice/src/main/java/org/example/Main.java
+++ DSA_Practice/src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.example.linkedlist.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LinkedList myLinkedList = new LinkedList(10);
+
+//        myLinkedList.printHead();
+//        myLinkedList.printTail();
+//        myLinkedList.printLength();
+
+        myLinkedList.append(20);
+        myLinkedList.printLinkedList();
     }
 }
