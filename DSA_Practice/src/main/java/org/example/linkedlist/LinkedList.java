38a857bb80d2345bccbde1a1f75700abfd62c776
poorvabhole <poorva.bhole@cognologix.com>
2024-07-16 15:50:34 +0530

    refactored-code

--- DSA_Practice/src/main/java/org/example/linkedlist/LinkedList.java
+++ DSA_Practice/src/main/java/org/example/linkedlist/LinkedList.java
@@ -5,44 +5,46 @@ public class LinkedList {
     private Node tail;
     private Integer length = 0;
 
-    class Node{
+    class Node {
         Integer value;
         Node next;
-        public Node(Integer value){
+
+        public Node(Integer value) {
             this.value = value;
         }
     }
 
-    public LinkedList(Integer value){
+    public LinkedList(Integer value) {
         Node newNode = new Node(value);
         head = newNode;
         tail = newNode;
         length = 1;
     }
-//    public void printHead(){
+
+    //    public void printHead(){
 //        System.out.println("Head --> "+ head.value);
 //    }
 //    public void printTail(){
 //        System.out.println("Tail --> "+ tail.value);
 //    }
-    public void printLength(){
-        System.out.println("Length --> "+ length);
+    public void printLength() {
+        System.out.println("Length --> " + length);
     }
 
-    public void printLinkedList(){
+    public void printLinkedList() {
         Node temp = head;
-        while (temp != null){
-            System.out.println("In printLinkedList method --> " +temp.value);
+        while (temp != null) {
+            System.out.println("In printLinkedList method --> " + temp.value);
             temp = temp.next;
         }
     }
 
-    public void append(Integer value){
+    public void append(Integer value) {
         Node newNode = new Node(value);
-        if (length == 0){
+        if (length == 0) {
             head = newNode;
             tail = newNode;
-        }else {
+        } else {
             tail.next = newNode;
             tail = newNode;
         }
